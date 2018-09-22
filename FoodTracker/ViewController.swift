5725763ea305a2bee7a974ce7fa698090900b816
Taiki Hisamura <taiki@fridaynight.co.jp>
2018-09-22 16:54:59 +0900

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Taiki Hisamura on 2018/09/22.
+//  Copyright © 2018年 Taiki Hisamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
